1486c52174c5fe3e5c2cb4e0df53c8a6e6d90617
garg-lucifer <naman33jii@gmail.com>
2021-07-03 16:32:41 +0530

    Commit

--- app/src/main/java/com/doodat/gfgcollapsing/MainActivity.java
+++ app/src/main/java/com/doodat/gfgcollapsing/MainActivity.java
@@ -3,8 +3,6 @@ package com.doodat.gfgcollapsing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
